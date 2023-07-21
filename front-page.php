0b0b79afa868ca2618b4ee90bf3e10fb3e8664b7
Eric Stefano <ericssm96@gmail.com>
2023-07-20 22:38:28 -0300

    Created and inserted standard post format template

--- front-page.php
+++ front-page.php
@@ -4,13 +4,7 @@
 if(have_posts()):
   while(have_posts()):the_post();
     ?>
-        <h1><?php the_title(); ?></h1>
-        <div class="thumbnail-img">
-            <?php the_post_thumbnail('thumbnail'); ?>
-        </div>
-        <p><?php the_content(); ?></p>
-        <small>Posted on <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
-        <br/>
+    <?php get_template_part('standard-post'); ?>
   <?php
   endwhile;
 endif;
--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php get_header(); ?>
 
-  <?php
-  if(have_posts()):
-    while(have_posts()):the_post();
+<?php
+if(have_posts()):
+  while(have_posts()):the_post();
     ?>
-    <h1><?php the_title(); ?></h1>
-    <p><?php the_content(); ?></p>
+    <?php get_template_part('standard-post'); ?>
   <?php
-    endwhile;
-  endif;
-    ?>
+  endwhile;
+endif;
+?>
 
 <?php get_footer(); ?>
