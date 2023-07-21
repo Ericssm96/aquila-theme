54e70b6825e6270be22a2223230897f6a5eb2c9f
Eric Stefano <ericssm96@gmail.com>
2023-07-20 23:27:47 -0300

    Set footer sidebar configs

--- functions.php
+++ functions.php
@@ -21,3 +21,16 @@ function aquila_theme_setup() {
 
 //add_action('after_setup_theme', 'aquila_theme_setup'); também é válido
 add_action('init', 'aquila_theme_setup');
+
+function aquila_widget_setup() {
+  register_sidebar([
+    'name' => 'My sidebar',
+    'id' => 'sidebar-1',
+    'class' => 'test',
+    'description' => 'footer sidebar',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' => '</aside>',
+    'before_title' => '<h1 class="widget-title">',
+    'after_title' => '</h1>'
+  ]);
+}
